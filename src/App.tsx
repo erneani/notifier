88c8e6920fe5bd46e739ffcb658e1118dd54e78c
Vinícius Ernani de Carvalho <viniciusernani@ufrn.edu.br>
2020-09-21 10:35:31 -0300

    Use effect to check notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 
 function App() {
+  const checkNotificationsAllowed = async () => {
+    await Notification.requestPermission();
+  };
+
+  useEffect(() => {
+    checkNotificationsAllowed();
+  }, []);
+
   return (
     <Container>
       <Card>
